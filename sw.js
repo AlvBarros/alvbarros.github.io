6a841ea08e42c9e805f01040910f32227432bbe2
AlvBarros <alv.barrosc@hotmail.com>
2019-11-21 00:24:14 -0300

    sw

--- sw.js
+++ sw.js
@@ -1,1 +1,17 @@
 console.log('registered!')
+
+self.addEventListener('install', function(e) {
+    e.waitUntil(
+        caches.open('airhorner').then(function(cache) {
+            return cache.addAll([
+                '/index.html',
+                '/offline.html'
+            ]);
+        })
+    )
+});
+
+self.addEventListener('fetch', function(e) {
+    console.log(e.request.url);
+});
+
