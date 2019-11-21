a779c21426ac511e4cda4d3588785adf89618e27
AlvBarros <alv.barrosc@hotmail.com>
2019-11-21 00:28:49 -0300

    sw

--- sw.js
+++ sw.js
@@ -11,7 +11,12 @@ self.addEventListener('install', function(e) {
     )
 });
 
-self.addEventListener('fetch', function(e) {
-    console.log(e.request.url);
+self.addEventListener('fetch', function(event) {
+    console.log(event.request.url);
+    event.respondWith(
+        caches.match(event.request).then(function(response) {
+            return response || fetch(event.request);
+        })
+    );
 });
 
