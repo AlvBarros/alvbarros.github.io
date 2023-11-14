bc4e22120209569dff955de1a9a0cf35dc499545
AlvBarros <alv.barrosc@hotmail.com>
2023-11-14 13:49:17 -0300

    fix: remove share on unsupported browsers

--- src/components/ShareProfile.tsx
+++ src/components/ShareProfile.tsx
@@ -3,19 +3,23 @@ import React from "react";
 import { faDownload, faPaperPlane } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
+declare var navigator: Navigator;
+
 export const ShareProfile = () => {
   return (
     <section className="ShareProfile">
-      <h1> ShareProfile my profile </h1>
-      <ul>
-        <li className="mobile-only clickable">
-          <FontAwesomeIcon icon={faPaperPlane} /> Share profile link
-        </li>
-        <li className="clickable">
-          <FontAwesomeIcon icon={faDownload} /> Download as PDF file
-        </li>
-        <li>[QR Code]</li>
-      </ul>
+      {navigator.canShare && navigator.canShare() ? (
+        <>
+          <h1> ShareProfile my profile </h1>
+          <ul>
+            <li className="mobile-only clickable">
+              <FontAwesomeIcon icon={faPaperPlane} /> Share profile link
+            </li>
+          </ul>
+        </>
+      ) : (
+        ""
+      )}
     </section>
   );
 };
