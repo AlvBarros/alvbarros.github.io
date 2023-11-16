da9a52a5720b2f47a6b07f4747c11a3ccd3c75a1
AlvBarros <alv.barrosc@hotmail.com>
2023-11-16 16:53:17 -0300

    refactor: hide share with css

--- src/components/ShareProfile.tsx
+++ src/components/ShareProfile.tsx
@@ -3,27 +3,21 @@ import React from "react";
 import { faDownload, faPaperPlane } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-declare var navigator: Navigator;
-
 export const ShareProfile = () => {
   return (
     <section className="ShareProfile">
-      <>
-        <h1> Share my profile </h1>
-        <ul>
-          <li className="mobile-only clickable">
-            <FontAwesomeIcon icon={faDownload} />
-            <a download href="/Alvaro_Barros.pdf">Download PDF resume</a>
-          </li>
-          {navigator.canShare && navigator.canShare() ? (
-            <li className="mobile-only clickable">
-              <FontAwesomeIcon icon={faPaperPlane} /> Share profile link
-            </li>
-          ) : (
-            ""
-          )}
-        </ul>
-      </>
+      <h1> Share my profile </h1>
+      <ul>
+        <li className="clickable">
+          <FontAwesomeIcon icon={faDownload} />
+          <a download href="/Alvaro_Barros.pdf">
+            Download PDF resume
+          </a>
+        </li>
+        <li className="mobile-only clickable">
+          <FontAwesomeIcon icon={faPaperPlane} /> Share profile link
+        </li>
+      </ul>
     </section>
   );
 };
